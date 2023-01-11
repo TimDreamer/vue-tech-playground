4f998dc2724a607930f880bc5ff435012c90e064
Tim Lu <tim.lu@linecrop.com>
2023-01-11 12:33:54 +0800

    fix

--- validate-form/src/mixins/emitter.js
+++ validate-form/src/mixins/emitter.js
@@ -3,9 +3,11 @@ export default {
     dispatch(componentName, eventName, ...payload) {
       let parent = this.$parent || this.$root
       let name = parent.$options.name
-      while (!parent || !name || name != componentName) {
-        if (parent) parent = parent.$parent
-        name = parent.$options.name
+      while (parent && (!name || name != componentName)) {
+        parent = parent.$parent
+        if (parent) {
+          name = parent.$options.name
+        }
       }
       if (parent && name === componentName) {
         parent.$emit.apply(parent, [eventName].concat(payload))
